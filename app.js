deedebb20c944f7cdfdbc83d3ee56ca92feda986
TetianaITstep <162435626+TetianaITstep@users.noreply.github.com>
2024-10-20 09:59:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function createElement(tag, { classes, attributes }, ...children) {
+  const element = document.createElement(tag);
+  if (classes) element.classList.add(...classes);
+
+  if (attributes) {
+    for (let key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
+
+  element.append(...children);
+  return element;
+}
+
+const calendar = document.querySelector(".calendar");
+
+function generateDays() {
+  for (let i = 1; i <= 31; i++) {
+    const day = createElement("div", {
+      classes: ["day_area"],
+    });
+    day.textContent = i;
+    calendar.appendChild(day);
+  }
+}
+
+generateDays();
