c543cd896c9705ff3afd7d7b948a100e9bc2b966
TetianaITstep <itsteptana@gmail.com>
2024-10-21 13:01:57 +0300

    days/months

--- app.js
+++ app.js
@@ -14,12 +14,29 @@ function createElement(tag, { classes, attributes }, ...children) {
 
 const calendar = document.querySelector(".calendar");
 
-function generateDays() {
-  for (let i = 1; i <= 31; i++) {
-    const day = createElement("div", {
-      classes: ["day_area"],
-    });
+// function generateDaysFirstVersion() {
+//   calendar.innerHTML = "";
+//   for (let i = 1; i <= 31; i++) {
+//     const day = createElement("div", {
+//       classes: ["day_area"],
+//     });
+//     day.textContent = i;
+//     calendar.appendChild(day);
+//     const today = new Date().getDate();
+// if (i === today) {
+//   day.classList.add("highlight");
+// }
+//   }
+// }
+
+// generateDaysFirstVersion();
+
+function generateDaysFor28() {
+  calendar.innerHTML = "";
+  for (let i = 1; i <= 28; i++) {
+    const day = createElement("div", { classes: ["day_area"] });
     day.textContent = i;
+
     calendar.appendChild(day);
     const today = new Date().getDate();
     if (i === today) {
@@ -28,11 +45,35 @@ function generateDays() {
   }
 }
 
-generateDays();
+function generateDaysFor29() {
+  calendar.innerHTML = "";
+  for (let i = 1; i <= 29; i++) {
+    const day = createElement("div", { classes: ["day_area"] });
+    day.textContent = i;
+    calendar.appendChild(day);
+  }
+}
+
+function generateDaysFor30() {
+  calendar.innerHTML = "";
+  for (let i = 1; i <= 30; i++) {
+    const day = createElement("div", { classes: ["day_area"] });
+    day.textContent = i;
+    calendar.appendChild(day);
+  }
+}
+
+function generateDaysFor31() {
+  calendar.innerHTML = "";
+  for (let i = 1; i <= 31; i++) {
+    const day = createElement("div", { classes: ["day_area"] });
+    day.textContent = i;
+    calendar.appendChild(day);
+  }
+}
 
 const monthName = document.getElementById("monthName");
-const prevMonthBtn = document.getElementById("prevMonth");
-const nextMonthBtn = document.getElementById("nextMonth");
+
 const monthArrow = [
   "January",
   "February",
@@ -49,6 +90,7 @@ const monthArrow = [
 ];
 
 let currentMonthIndex = new Date().getMonth();
+let currentYear = new Date().getFullYear();
 function changemonthName() {
   const currentMonth = monthArrow.find(
     (month, index) => index === currentMonthIndex
@@ -59,7 +101,37 @@ function changemonthName() {
 changemonthName();
 
 //!задачі на наст раз:
-//додати події на кнопки для місяців
-//поєднати дні з місяцями, щоб змінювалось і то і то
+//поєднати дні з місяцями, щоб змінювалось і то і то - переробити логіку
 //прикрутити роки
 //функція для відкриття вікна, при натисканні на день
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+function generateDays() {
+  if (currentMonthIndex === 1) {
+    if (isLeapYear(currentYear)) {
+      generateDaysFor29();
+    } else {
+      generateDaysFor28();
+    }
+  } else if ([3, 5, 8, 10].includes(currentMonthIndex)) {
+    generateDaysFor30();
+  } else {
+    generateDaysFor31();
+  }
+}
+
+const prevMonthBtn = document.getElementById("prevMonth");
+prevMonthBtn.addEventListener("click", () => {
+  currentMonthIndex--;
+  if (currentMonthIndex < 0) currentMonthIndex = 11;
+  changemonthName();
+  generateDays();
+});
+const nextMonthBtn = document.getElementById("nextMonth");
+nextMonthBtn.addEventListener("click", () => {
+  currentMonthIndex++;
+  if (currentMonthIndex > 11) currentMonthIndex = 0;
+  changemonthName();
+  generateDays();
+});
